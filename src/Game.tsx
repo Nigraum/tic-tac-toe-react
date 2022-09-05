cbd9811857b32c16ae68c06ea311778fb43d65a4
Nigraum <yan.corintima@hotmail.com>
2022-09-05 11:23:33 -0300

    Fixing tailwind installation

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,7 @@
 function Game() {
   return (
-    <div>
-      Tic Tac Toe Game
+    <div className="h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe Game</h1>
     </div>
   );
 };
