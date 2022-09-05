b13fe91da9efe9eae14b141ba454eb74f0e78ec0
Nigraum <yan.corintima@hotmail.com>
2022-09-05 11:26:12 -0300

    Fixing gradient

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,11 @@
 function Game() {
   return (
-    <div className="h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe Game</h1>
+      <div>
+        Board Goes Here
+      </div>
+      <div>Scores Go Here</div>
     </div>
   );
 };
