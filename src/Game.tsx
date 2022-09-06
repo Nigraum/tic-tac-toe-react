703b54d6c79e9298876b6063f32c966e0dde927d
Nigraum <yan.corintima@hotmail.com>
2022-09-06 12:38:09 -0300

    Create game state const and importing useState

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", "", ]
+
 function Game() {
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe Game</h1>
       <div>
-        Board Goes Here
+        <div>
+          {INITIAL_GAME_STATE.map((player, index) => (
+            <div>{player}</div>
+          ))}
+        </div>
+        <div>Scores Go Here</div>
       </div>
-      <div>Scores Go Here</div>
     </div>
   );
 };
