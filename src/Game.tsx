8344dcbaf9f881fe5862396942c888b990760567
Nigraum <yan.corintima@hotmail.com>
2022-09-06 12:43:43 -0300

    Create grid

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
-const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", "", ]
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", "", ];
 
 function Game() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe Game</h1>
       <div>
-        <div>
-          {INITIAL_GAME_STATE.map((player, index) => (
-            <div>{player}</div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {gameState.map((player, index) => (
+            <div key={index} className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">{player}</div>
           ))}
         </div>
         <div>Scores Go Here</div>
