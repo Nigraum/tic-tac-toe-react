97805bd9119afb88b0d282c5263770e04ae693cb
Nigraum <yan.corintima@hotmail.com>
2022-09-07 20:09:54 -0300

    Styling square

--- src/square.tsx
+++ src/square.tsx
@@ -8,8 +8,8 @@ function Square({ player }: Props) {
   const hoverStyle = "transition duration-500 hover:scale-105 transform"
 
   return (
-    <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
-      <span>{player}</span>
+    <div className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}>
+      <span className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}>{player}</span>
     </div>
   );
 };
