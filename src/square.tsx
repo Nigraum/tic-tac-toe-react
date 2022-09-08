6e8bb07a144fc1dac58357286cd54f443b175de7
Nigraum <yan.corintima@hotmail.com>
2022-09-07 21:09:53 -0300

    Create new props

--- src/square.tsx
+++ src/square.tsx
@@ -1,15 +1,23 @@
 type Props = {
+  index: string;
+  onClick(): void;
   player?: string;
 };
 
-function Square({ player }: Props) {
+function Square({ index, onClick, player }: Props) {
   const scale = player ? "scale-100" : "scale-0"
   const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300"
   const hoverStyle = "transition duration-500 hover:scale-105 transform"
 
   return (
-    <div className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}>
-      <span className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}>{player}</span>
+    <div 
+    data-cell-index={index}
+    className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+    {...{ onClick }}
+    > 
+      <span 
+      data-cell-index={index}
+      className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}>{player}</span>
     </div>
   );
 };
