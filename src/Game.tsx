014db91b3d7450a8ebdee8a94a32b06d50c1c3ea
Nigraum <yan.corintima@hotmail.com>
2022-09-14 01:41:45 -0300

    Styling paragraphs of scores div

--- src/Game.tsx
+++ src/Game.tsx
@@ -112,9 +112,15 @@ function Game() {
         </div>
 
         <div className="mx-auto w-96 text-2xl text-serif">
-            <p>Next Player: <span>{currentPlayer}</span></p>
-            <p>Player X wins: <span>{scores["X"]}</span></p>
-            <p>Player O wins: <span>{scores["O"]}</span></p>
+            <p className="text-white mt-5">
+              Next Player: <span>{currentPlayer}</span>
+            </p>
+            <p className="text-white mt-5">
+              Player X wins: <span>{scores["X"]}</span>
+            </p>
+            <p className="text-white mt-5">
+              Player O wins: <span>{scores["O"]}</span>
+            </p>
         </div>
       </div>
     </div>
