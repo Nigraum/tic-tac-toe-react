7cd43a9387e1a34a07d6ec9cc7dd35c5dcf91412
Nigraum <yan.corintima@hotmail.com>
2022-09-14 01:39:37 -0300

    Setting players scores

--- src/Game.tsx
+++ src/Game.tsx
@@ -113,6 +113,8 @@ function Game() {
 
         <div>
             <p>Next Player: <span>{currentPlayer}</span></p>
+            <p>Player X wins: <span>{scores["X"]}</span></p>
+            <p>Player O wins: <span>{scores["O"]}</span></p>
         </div>
       </div>
     </div>
